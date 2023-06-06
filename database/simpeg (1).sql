427284a4c865df198480c46701258aee737e00cb
syariflldikti11 <108456492+syariflldikti11@users.noreply.github.com>
2023-06-06 09:02:53 +0800

    aa

--- database/simpeg (1).sql
+++ database/simpeg (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Bulan Mei 2023 pada 17.07
+-- Waktu pembuatan: 06 Jun 2023 pada 02.58
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.2.31
 
@@ -49,7 +49,7 @@ INSERT INTO `arsip_kepegawaian` (`id_arsip_kepegawaian`, `id_jenis_arsip`, `kete
 
 CREATE TABLE `arsip_pegawai` (
   `id_arsip_pegawai` char(100) NOT NULL,
-  `nik` char(100) NOT NULL,
+  `id_pegawai` char(100) NOT NULL,
   `id_jenis_arsip` char(100) NOT NULL,
   `keterangan` char(100) NOT NULL,
   `file` char(100) NOT NULL
@@ -106,6 +106,14 @@ CREATE TABLE `keluarga` (
   `tgl_lahir_keluarga` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `keluarga`
+--
+
+INSERT INTO `keluarga` (`id_keluarga`, `id_pegawai`, `jenis_keluarga`, `nama_keluarga`, `tempat_lahir_keluarga`, `tgl_lahir_keluarga`) VALUES
+('126cbac5-fef2-11ed-9466-15929c57de55', 'ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', 'Ibu', 'khai', 'jaro', '2023-05-31'),
+('229f1c09-fef2-11ed-9466-15929c57de55', 'ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', 'Ibu', 'Amrullah', 'jaro', '2023-05-30');
+
 -- --------------------------------------------------------
 
 --
@@ -114,6 +122,7 @@ CREATE TABLE `keluarga` (
 
 CREATE TABLE `pegawai` (
   `id_pegawai` char(100) NOT NULL,
+  `nama_pegawai` char(100) NOT NULL,
   `nip` char(100) NOT NULL,
   `nik` char(100) NOT NULL,
   `kk` char(100) NOT NULL,
@@ -134,8 +143,8 @@ CREATE TABLE `pegawai` (
 -- Dumping data untuk tabel `pegawai`
 --
 
-INSERT INTO `pegawai` (`id_pegawai`, `nip`, `nik`, `kk`, `tempat_lahir`, `tgl_lahir`, `alamat`, `agama`, `email`, `no_hp`, `status_kawin`, `id_jabatan`, `pendidikan_terakhir`, `tmt`, `file`) VALUES
-('ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', '197101161992032001', '6309112608980002', '123', 'Tabalong', '2023-05-28', 'handil bakti', 'Islamm', 'mujib@gmail.com', '085849658066', 'Menikah', 'c65b69e3-fc65-11ed-9f05-ea3c6f7707bf', 'D3', '2023-05-28', 'SIM_A.jpg');
+INSERT INTO `pegawai` (`id_pegawai`, `nama_pegawai`, `nip`, `nik`, `kk`, `tempat_lahir`, `tgl_lahir`, `alamat`, `agama`, `email`, `no_hp`, `status_kawin`, `id_jabatan`, `pendidikan_terakhir`, `tmt`, `file`) VALUES
+('ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', 'Syarif', '197101161992032001', '6309112608980001', '123456', 'Tabalong', '2023-05-28', 'handil bakti', 'Islamm', 'mujib@gmail.com', '085849658066', 'Menikah', '--Pilih Jabatan--', 'D3', '2023-05-28', 'WIN_20220727_15_31_35_Pro1.jpg');
 
 -- --------------------------------------------------------
 
@@ -145,7 +154,7 @@ INSERT INTO `pegawai` (`id_pegawai`, `nip`, `nik`, `kk`, `tempat_lahir`, `tgl_la
 
 CREATE TABLE `pendidikan` (
   `id_pendidikan` char(100) NOT NULL,
-  `nik` char(100) NOT NULL,
+  `id_pegawai` char(100) NOT NULL,
   `jenjang` char(100) NOT NULL,
   `nama_sekolah` char(100) NOT NULL,
   `jurusan` char(100) NOT NULL,
@@ -153,13 +162,33 @@ CREATE TABLE `pendidikan` (
   `no_ijazah` char(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `pendidikan`
+--
+
+INSERT INTO `pendidikan` (`id_pendidikan`, `id_pegawai`, `jenjang`, `nama_sekolah`, `jurusan`, `tahun_lulus`, `no_ijazah`) VALUES
+('65311ed3-fed8-11ed-9b2c-df0c9ef3e9b5', 'ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', 'SD', 'SDN 1 Jaro', 'sss', '2001', 'aa');
+
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `pengguna`
 --
--- Kesalahan membaca struktur untuk tabel simpeg.pengguna: #1932 - Table 'simpeg.pengguna' doesn't exist in engine
--- Kesalahan membaca data untuk tabel simpeg.pengguna: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `simpeg`.`pengguna`' at line 1
+
+CREATE TABLE `pengguna` (
+  `id_pengguna` char(100) NOT NULL,
+  `id_pegawai` char(100) NOT NULL,
+  `password` char(100) NOT NULL,
+  `role` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengguna`
+--
+
+INSERT INTO `pengguna` (`id_pengguna`, `id_pegawai`, `password`, `role`) VALUES
+('00921d03-fee5-11ed-9466-15929c57de55', 'ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', '21232f297a57a5a743894a0e4a801fc3', 2),
+('0f783355-fee8-11ed-9466-15929c57de55', 'ec3f5a25-fd64-11ed-9f05-ea3c6f7707bf', '827ccb0eea8a706c4c34a16891f84e7b', 1);
 
 --
 -- Indexes for dumped tables
@@ -206,6 +235,12 @@ ALTER TABLE `pegawai`
 --
 ALTER TABLE `pendidikan`
   ADD PRIMARY KEY (`id_pendidikan`);
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id_pengguna`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
